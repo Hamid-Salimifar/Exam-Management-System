2422af0c2b1392f4330bd822e48cb3f2a42838c8
Hamid-Salimifar <hamidsalimifar618@gmail.com>
2025-12-14 18:58:53 +0330

    feat (RoleService): add findByName method in RoleService and its implementation

--- src/main/java/com/example/exammanagementsystem/service/RoleService.java
+++ src/main/java/com/example/exammanagementsystem/service/RoleService.java
@@ -1,6 +1,10 @@
 package com.example.exammanagementsystem.service;
 
 import com.example.exammanagementsystem.model.Role;
+import com.example.exammanagementsystem.model.RoleName;
+
+import java.util.Optional;
 
 public interface RoleService extends BaseService<Role>{
+    Optional<Role> findByName(RoleName name);
 }
--- src/main/java/com/example/exammanagementsystem/service/impl/RoleServiceImpl.java
+++ src/main/java/com/example/exammanagementsystem/service/impl/RoleServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.exammanagementsystem.service.impl;
 
 import com.example.exammanagementsystem.model.Role;
+import com.example.exammanagementsystem.model.RoleName;
 import com.example.exammanagementsystem.repository.RoleRepository;
 import com.example.exammanagementsystem.service.RoleService;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,7 +9,14 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class RoleServiceImpl extends BaseServiceImpl<Role> implements RoleService {
+    private final RoleRepository roleRepository;
     public RoleServiceImpl(RoleRepository roleRepository) {
         super(roleRepository);
+        this.roleRepository=roleRepository;
+    }
+
+    @Override
+    public Role findByName(RoleName name) {
+       return roleRepository.findByName(name);
     }
 }
