336641c69df46168d207f1da63b691af11c81a4b
Hamid-Salimifar <hamidsalimifar618@gmail.com>
2025-12-14 19:09:56 +0330

    feat (userRepository): make the findByUsername return optional

--- src/main/java/com/example/exammanagementsystem/repository/UserRepository.java
+++ src/main/java/com/example/exammanagementsystem/repository/UserRepository.java
@@ -4,9 +4,10 @@ import com.example.exammanagementsystem.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
 
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 
 }
