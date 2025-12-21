ee4f9a98585d061992b6ef47c0a007944e5b410d
Hamid-Salimifar <hamidsalimifar618@gmail.com>
2025-12-21 13:26:11 +0330

    feat (User) : add findByRole method in user repository

--- src/main/java/com/example/exammanagementsystem/repository/UserRepository.java
+++ src/main/java/com/example/exammanagementsystem/repository/UserRepository.java
@@ -1,13 +1,19 @@
 package com.example.exammanagementsystem.repository;
 
+import com.example.exammanagementsystem.model.RoleName;
 import com.example.exammanagementsystem.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User,Long>, JpaSpecificationExecutor<User> {
 
     Optional<User> findByUsername(String username);
+
+    List<User> findByRoles_Name(RoleName roleName);
+
 
 }
