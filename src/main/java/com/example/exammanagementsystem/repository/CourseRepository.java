d0d0def21a45fd71664ef97ca165ed9e3a6047f8
Hamid-Salimifar <hamidsalimifar618@gmail.com>
2025-12-22 11:51:09 +0330

    feat (Course): add findByTeacher method in CourseRepository

--- src/main/java/com/example/exammanagementsystem/repository/CourseRepository.java
+++ src/main/java/com/example/exammanagementsystem/repository/CourseRepository.java
@@ -1,7 +1,11 @@
 package com.example.exammanagementsystem.repository;
 
 import com.example.exammanagementsystem.model.Course;
+import com.example.exammanagementsystem.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CourseRepository extends JpaRepository<Course,Long> {
+    List<Course> findByTeacher(User teacher);
 }
