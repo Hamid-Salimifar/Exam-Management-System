11b55eea04a076659c1f9c548edc51e05418d5c5
Hamid-Salimifar <hamidsalimifar618@gmail.com>
2025-12-22 11:58:30 +0330

    feat (Exam): add ExamRepository with findByTeacher_Id and findByCourseAndTeacher

---
+++ src/main/java/com/example/exammanagementsystem/repository/ExamRepository.java
@@ -0,0 +1,12 @@
+package com.example.exammanagementsystem.repository;
+
+import com.example.exammanagementsystem.model.Exam;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ExamRepository extends JpaRepository<Exam,Long> {
+    List<Exam> findByTeacher_Id(Long teacherId);
+
+    List<Exam> findByCourse_IdAndTeacher_Id(Long courseId, Long teacherId);
+}
