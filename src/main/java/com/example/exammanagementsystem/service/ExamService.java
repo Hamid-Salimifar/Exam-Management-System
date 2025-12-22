2a19e589bb8cccae328ee4d65ab9ffbb4d28fc7c
Hamid-Salimifar <hamidsalimifar618@gmail.com>
2025-12-22 11:59:01 +0330

    feat (Exam): add ExamService with findByTeacher_Id and findByCourseAndTeacher and implement them in ExamServiceImpl

---
+++ src/main/java/com/example/exammanagementsystem/service/ExamService.java
@@ -0,0 +1,14 @@
+package com.example.exammanagementsystem.service;
+
+import com.example.exammanagementsystem.model.Exam;
+
+import java.util.List;
+
+public interface ExamService extends BaseService<Exam>{
+
+    List<Exam> findByTeacher_Id(Long teacherId);
+
+    List<Exam> findByCourse_IdAndTeacher_Id(Long courseId, Long teacherId);
+
+    Exam updateExam(Long id, Exam updatedExam);
+}
---
+++ src/main/java/com/example/exammanagementsystem/service/impl/ExamServiceImpl.java
@@ -0,0 +1,43 @@
+package com.example.exammanagementsystem.service.impl;
+
+import com.example.exammanagementsystem.model.Exam;
+import com.example.exammanagementsystem.repository.ExamRepository;
+import com.example.exammanagementsystem.service.ExamService;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ExamServiceImpl extends BaseServiceImpl<Exam> implements ExamService {
+
+    private  final ExamRepository  examRepository;
+    public ExamServiceImpl(ExamRepository examRepository) {
+        super(examRepository);
+        this.examRepository=examRepository;
+    }
+
+    @Override
+    public List<Exam> findByTeacher_Id(Long teacherId) {
+        return examRepository.findByTeacher_Id(teacherId);
+    }
+
+    @Override
+    public List<Exam> findByCourse_IdAndTeacher_Id(Long courseId, Long teacherId) {
+       return examRepository.findByCourse_IdAndTeacher_Id(courseId,teacherId);
+    }
+
+    @Override
+    public Exam updateExam(Long id, Exam updatedExam) {
+        Exam exam = examRepository.findById(updatedExam.getId()).orElseThrow();
+        exam.setTitle(updatedExam.getTitle());
+        exam.setDescription(updatedExam.getDescription());
+        exam.setStartTime(updatedExam.getStartTime());
+        exam.setEndTime(updatedExam.getEndTime());
+        exam.setDurationMinutes(updatedExam.getDurationMinutes());
+
+        return examRepository.save(exam);
+
+
+    }
+}
