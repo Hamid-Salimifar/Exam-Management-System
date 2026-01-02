6ee5bcfe5d23c38e9eef9c195bfe1a1a4690d094
Hamid-Salimifar <hamidsalimifar618@gmail.com>
2026-01-02 12:56:42 +0330

    feat (McqQuestion): add McqQuestion entity which extend Question

---
+++ src/main/java/com/example/exammanagementsystem/model/McqQuestion.java
@@ -0,0 +1,23 @@
+package com.example.exammanagementsystem.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class McqQuestion extends Question {
+
+
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Option> options=new ArrayList<>();
+
+}
