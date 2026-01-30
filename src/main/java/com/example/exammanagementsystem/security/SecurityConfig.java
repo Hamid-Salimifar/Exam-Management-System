d6e289f6e38bc12dbd77a1d0c288b6d2393e31e2
Hamid-Salimifar <hamidsalimifar618@gmail.com>
2026-01-30 11:22:11 +0330

    feat: define a path only for Student role

--- src/main/java/com/example/exammanagementsystem/security/SecurityConfig.java
+++ src/main/java/com/example/exammanagementsystem/security/SecurityConfig.java
@@ -52,6 +52,7 @@ public class SecurityConfig {
                         .requestMatchers("/register/**", "/login","/css/**").permitAll()
                         .requestMatchers("/manager/**").hasRole("MANAGER")
                         .requestMatchers("/teacher/**").hasRole("TEACHER")
+                        .requestMatchers("/student/**").hasRole("STUDENT")
                         .anyRequest().authenticated())
                 .formLogin(form->form
                         .loginPage("/login")
